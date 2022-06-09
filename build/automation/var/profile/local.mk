c46d3e90e0e38565010f6a6dbadabb7ab085cbf8
Tamara Goldschmidt <77108863+tamaragoldschmidt@users.noreply.github.com>
2022-06-09 08:26:27 +0100

    DPTS-123 Adding in sonarcloud (#11)
    * DPTS-123 Adding in sonarcloud

---
+++ build/automation/var/profile/local.mk
@@ -0,0 +1,6 @@
+-include $(VAR_DIR)/profile/nonprod.mk # To allow for docker build to work correct
+
+# ==============================================================================
+# Service variables
+
+TASKS := referralroles
