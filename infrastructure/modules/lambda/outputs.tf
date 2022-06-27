4bf738b65ade308ade1015e3391cd7558e0eb62c
Tim Rickwood <tim.rickwood@nhs.net>
2022-06-27 14:47:04 +0100

    DPTS-505 Add cloudwatch event permission to trigger lambda

--- infrastructure/modules/lambda/outputs.tf
+++ infrastructure/modules/lambda/outputs.tf
@@ -9,3 +9,7 @@ output "lambda_qualified_arn" {
 output "lambda_version" {
   value = aws_lambda_function.lambda.version
 }
+
+output "lambda_function_name" {
+  value = aws_lambda_function.lambda.function_name
+}
