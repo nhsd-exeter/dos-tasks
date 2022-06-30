d2990b63da2ce1679afef04a1946970d31e5e482
Tim Rickwood <tim.rickwood@nhs.net>
2022-06-30 07:30:19 +0100

    DPTS-505 Add cron name to lambda name

--- infrastructure/stacks/ragreset/template/main.tf
+++ infrastructure/stacks/ragreset/template/main.tf
@@ -1,6 +1,6 @@
 module "ragreset_lambda" {
   source             = "../../modules/lambda"
-  name               = "cron-ragreset"
+  name               = "cron-ragreset-DB_NAME_TO_REPLACE"
   image_uri          = "${var.aws_lambda_ecr}/${var.project_group_short}/${var.project_name_short}/cron-ragreset:${var.image_version}"
   subnet_ids         = [data.terraform_remote_state.vpc.outputs.private_subnets[0], data.terraform_remote_state.vpc.outputs.private_subnets[1], data.terraform_remote_state.vpc.outputs.private_subnets[2]]
   security_group_ids = [data.terraform_remote_state.security_groups.outputs.lambda_security_group_id]
