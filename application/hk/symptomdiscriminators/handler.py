60baa1dd77a22235315ab58b542cb8a279eb4b88
Tamara <tamara.goldschmidt@nhs.net>
2022-07-04 11:27:05 +0100

    DPTS-569 Missed one action

--- application/hk/symptomdiscriminators/handler.py
+++ application/hk/symptomdiscriminators/handler.py
@@ -78,9 +78,9 @@ def process_extracted_data(db_connection, row_data, summary_count_dict, event, s
                 query, data = generate_db_query(row_values, event, start)
                 database.execute_db_query(db_connection, query, data, row_number, row_values, summary_count_dict)
             else:
-                common.increment_summary_count(summary_count_dict, {"action": "ERROR"})
+                common.increment_summary_count(summary_count_dict,  "ERROR")
         except Exception as e:
-            common.increment_summary_count(summary_count_dict, {"action": "ERROR"})
+            common.increment_summary_count(summary_count_dict, "ERROR")
             logger.log_for_error(
                 "Processing {0} data failed with |{1}|{2}| => {3}".format(
                     task_description, row_values["id"], row_values["name"], str(e)
