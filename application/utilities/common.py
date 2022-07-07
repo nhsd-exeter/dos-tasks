b9e3f0164f4384ddd1479efed1968c4668e24704
Tim Rickwood <tim.rickwood@nhs.net>
2022-07-07 16:12:39 +0100

    DPTS-511 Name value pairs for logging

--- application/utilities/common.py
+++ application/utilities/common.py
@@ -15,7 +15,7 @@ def check_csv_format(csv_row, csv_column_count, env):
     if len(csv_row) == csv_column_count:
         return True
     else:
-        log_for_audit(env, "CSV format invalid - invalid length")
+        log_for_audit(env, "action:validation | Problem:CSV format invalid - invalid length")
         return False
 
 
@@ -33,7 +33,7 @@ def valid_action(record_exists, row_data, env):
 
 def cleanup(db_connection, bucket, filename, event, start, summary_count_dict):
     # Close DB connection
-    log_for_audit(event["env"], "Closing DB connection...")
+    log_for_audit(event["env"], "action: close DB connection")
     db_connection.close()
     # Archive file
     s3_class = utilities.s3.S3()
@@ -41,16 +41,16 @@ def cleanup(db_connection, bucket, filename, event, start, summary_count_dict):
     s3_class.delete_object(bucket, filename, event, start)
     log_for_audit(
         event["env"],
-        "Archived file {} to {}/archive/{}".format(filename, filename.split("/")[0], filename.split("/")[1]),
+        "action:archive file:{} | bucket:{}/archive/{}".format(filename, filename.split("/")[0], filename.split("/")[1]),
     )
     # Send Slack Notification
-    log_for_audit(event["env"], "Sending slack message...")
+    log_for_audit(event["env"], "action:task complete")
     utilities.message.send_success_slack_message(event, start, summary_count_dict)
     return "Cleanup Successful"
 
 
 def retrieve_file_from_bucket(bucket, filename, event, start):
-    log_for_audit(event["env"], "Looking in {} for {} file".format(bucket, filename))
+    log_for_audit(event["env"], "action:retrieve file | bucket:{} | file:{}".format(bucket, filename))
     s3_bucket = utilities.s3.S3()
     return s3_bucket.get_object(bucket, filename, event, start)
 
@@ -61,13 +61,13 @@ def check_csv_values(line, env):
     try:
         int(line[0])
     except ValueError:
-        log_for_audit(env, "Id {} must be a integer".format(line[0]))
+        log_for_audit(env, "action:validation | Problem:Id {} must be a integer".format(line[0]))
         valid_values = False
     if not str(line[0]):
-        log_for_audit(env, "Id {} can not be null or empty".format(line[0]))
+        log_for_audit(env, "action:validation | Problem:Id {} can not be null or empty".format(line[0]))
         valid_values = False
     if not line[1]:
-        log_for_audit(env, "Name/Description {} can not be null or empty".format(line[1]))
+        log_for_audit(env, "action:validation | Problem:Name/Description {} can not be null or empty".format(line[1]))
         valid_values = False
     return valid_values
 
@@ -114,7 +114,7 @@ def process_file(csv_file, event, start, expected_col_count, summary_count_dict)
             increment_summary_count(summary_count_dict, "ERROR", event["env"])
             log_for_audit(
                 event["env"],
-                "Incorrect line format on line {0}, should be {1} but is {2}".format(
+                "action:Incorrect line format | line: {0} | expected:{1} | actual:{2}".format(
                     count, expected_col_count, len(line)
                 ),
             )
