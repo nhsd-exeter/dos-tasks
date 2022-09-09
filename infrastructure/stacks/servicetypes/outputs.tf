6c2df6fbf27d94f7250a39b1a676360a8d6469a2
Tamara Goldschmidt <tamara.goldschmidt@nhs.net>
2022-09-09 15:36:40 +0100

    DPTS-793 Removing double s mispelling

--- infrastructure/stacks/servicetypes/outputs.tf
+++ infrastructure/stacks/servicetypes/outputs.tf
@@ -1,6 +1,6 @@
 output "servicetypes_lambda_arn" {
   description = "ARN value for the HK Symptom Discriminators Lambda function"
-  value       = module.sservicetypes_lambda.lambda_arn
+  value       = module.servicetypes_lambda.lambda_arn
 }
 
 output "servicetypes_lambda_qualified_arn" {
