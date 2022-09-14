dcb01148734844be84e29d2ae9bb78aecb1f5198
Tim Rickwood <tim.rickwood@nhs.net>
2022-09-14 16:30:54 +0100

    DPTS-510 Poc integration profile and jenkins

---
+++ build/automation/var/profile/integration.mk
@@ -0,0 +1,29 @@
+-include $(VAR_DIR)/platform-texas/v1/account-live-k8s-nonprod.mk
+
+# ==============================================================================
+# Service variables
+
+PROJECT_IMAGE_TAG :=
+ENV := integration
+ENVIRONMENT := integration
+SERVICE_PREFIX := $(PROJECT_ID)-$(ENV)
+VULNERABILITY_LEVEL := CRITICAL,HIGH
+
+# ==============================================================================
+# Infrastructure variables
+
+STACKS := secrets,s3,security-groups
+# TODO restore  referralroles - temp out to avoid cross-cutting other work
+TASKS := filter,referralroles,symptomgroups,symptomdiscriminators
+ENVIRONMENT_LIST := ["integration"]
+TF_VAR_environment_list := $(ENVIRONMENT_LIST)
+
+TF_VAR_deployment_secrets := $(DEPLOYMENT_SECRETS)
+TF_VAR_image_version := $(or $(BUILD_TAG), latest)
+TF_VAR_s3_tf_state_key := $(PROJECT_ID)/$(ENV)/s3/terraform.state
+TF_VAR_security_groups_tf_state_key := $(PROJECT_ID)/$(ENV)/security-groups/terraform.state
+TF_VAR_core_dos_db_sg := sg-05fdf44634b750fbd
+TF_VAR_splunk_firehose_subscription := dos-cw-w-events-logs-firehose
+TF_VAR_splunk_firehose_role := dos_cw_w_events_firehose_access_role
+
+LAMBDA_VERSIONS_TO_RETAIN = 5
