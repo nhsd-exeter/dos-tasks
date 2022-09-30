b842872db5ac805ff795e9644e07e21506f256a2
Tim Rickwood <tim.rickwood@nhs.net>
2022-09-30 14:00:26 +0100

    DPTS-824 Wip correct output refs to module

--- infrastructure/stacks/integration-test/outputs.tf
+++ infrastructure/stacks/integration-test/outputs.tf
@@ -1,12 +1,12 @@
-output "integrationtest_lambda_arn" {
+output "integration_test_lambda_arn" {
   description = "ARN value for the HK integration test Lambda function"
-  value       = module.integrationtest_lambda.lambda_arn
+  value       = module.integration_test_lambda.lambda_arn
 }
 
-output "integrationtest_lambda_qualified_arn" {
-  value = module.integrationtest_lambda.lambda_qualified_arn
+output "integration_test_lambda_qualified_arn" {
+  value = module.integration_test_lambda.lambda_qualified_arn
 }
 
-output "integrationtest_lambda_version" {
-  value = module.integrationtest_lambda.lambda_version
+output "integration_test_lambda_version" {
+  value = module.integration_test_lambda.lambda_version
 }
