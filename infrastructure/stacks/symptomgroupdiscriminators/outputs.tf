8ff6db2b3ce7f639fdc0ef64f2be853437915949
Tamara Goldschmidt <tamara.goldschmidt@nhs.net>
2022-10-03 11:37:29 +0100

    DPTS-584 Minor name changes

--- infrastructure/stacks/symptomgroupdiscriminators/outputs.tf
+++ infrastructure/stacks/symptomgroupdiscriminators/outputs.tf
@@ -1,5 +1,5 @@
 output "symptomgroupdiscriminators_lambda_arn" {
-  description = "ARN value for the HK Symptom Group Symptom Discriminators Lambda function"
+  description = "ARN value for the HK Symptom Group Discriminators Lambda function"
   value       = module.symptomgroupdiscriminators_lambda.lambda_arn
 }
 
