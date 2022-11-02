5df76feee43824432b97a73ec453bc1e8191eaff
Tim Rickwood <tim.rickwood@nhs.net>
2022-11-02 07:54:28 +0000

    DPTS-893 Post review changes and update put and live profiles

--- build/automation/var/profile/live.mk
+++ build/automation/var/profile/live.mk
@@ -19,8 +19,10 @@ TF_VAR_deployment_secrets := $(DEPLOYMENT_SECRETS)
 TF_VAR_image_version := $(or $(BRANCH_NAME), latest)
 TF_VAR_s3_tf_state_key := $(PROJECT_ID)/$(ENV)/s3/terraform.state
 TF_VAR_security_groups_tf_state_key := $(PROJECT_ID)/$(ENV)/security-groups/terraform.state
-TF_VAR_core_dos_db_sg := sg-0019e059390d78db7
 TF_VAR_splunk_firehose_subscription := dos-cw-w-events-logs-firehose
 TF_VAR_splunk_firehose_role := dos_cw_w_events_firehose_access_role
 
 LAMBDA_VERSIONS_TO_RETAIN = 5
+
+# Derive the security group for RDS
+TF_VAR_db_identifier = uec-core-dos-live-db-12
--- infrastructure/stacks/security-groups/variables.tf
+++ infrastructure/stacks/security-groups/variables.tf
@@ -10,10 +10,6 @@ variable "vpc_terraform_state_key" {
 # # SG
 # ##############
 
-# variable "core_dos_db_sg" {
-#   description = "The ID of the Core DoS sg database"
-# }
-
 variable "db_identifier" {
   description = "Identifer of RDS instance from which to extract the security group"
 }
