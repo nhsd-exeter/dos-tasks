01d8ea34e6d713b9d3736306109ebd2fff172320
Tamara Goldschmidt <tamara.goldschmidt@nhs.net>
2022-11-09 14:23:26 +0000

    DPTS-892 Adding in failure tests

--- infrastructure/stacks/secrets/main.tf
+++ infrastructure/stacks/secrets/main.tf
@@ -2,3 +2,8 @@ resource "aws_secretsmanager_secret" "deployment_secrets" {
   name = "${var.service_prefix}/deployment"
   tags = local.standard_tags
 }
+
+resource "aws_secretsmanager_secret" "slack_secrets" {
+  name = "${var.service_prefix}/slack"
+  tags = local.standard_tags
+}
