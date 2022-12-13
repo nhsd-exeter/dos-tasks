d947e1e93324a83d0ba7c4e68e1c295925447161
Tim Rickwood <tim.rickwood@nhs.net>
2022-12-13 13:37:10 +0000

    DPTS-985 Remove tf state for cron and sg

--- infrastructure/stacks/cron-template/ragreset/variables.tf
+++ infrastructure/stacks/cron-template/ragreset/variables.tf
@@ -2,24 +2,12 @@
 # # LAMBDA
 # ##############
 
-variable "s3_tf_state_key" {
-  description = "State store for s3 stack terraform"
-}
-
 variable "image_version" {
   description = "The version of the Lambda docker image"
 }
 
 variable "aws_ecr" {
   description = "ECR repository to store lambda docker images"
-}
-
-variable "vpc_terraform_state_key" {
-  description = "State store for VPC stack terraform"
-}
-
-variable "security_groups_tf_state_key" {
-  description = "State store for security groups stack terraform"
 }
 
 variable "slack_webhook_url" {
