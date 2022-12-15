6e2befb3517813e853821902ab1f8dd423294e6e
Tim Rickwood <tim.rickwood@nhs.net>
2022-12-15 11:48:43 +0000

    DPTS-985 Remove terraform state use from devops templates

--- build/automation/lib/terraform/template/data-texas-v1.tf
+++ build/automation/lib/terraform/template/data-texas-v1.tf
@@ -1,42 +1,57 @@
 # ==============================================================================
 # Data
 
-data "terraform_remote_state" "eks" {
-  backend = "s3"
-  config = {
-    key    = var.eks_terraform_state_key
-    bucket = var.terraform_platform_state_store
-    region = var.aws_region
-  }
-}
+# Not used but replace with data aws_eks_cluster ?
+# data "terraform_remote_state" "eks" {
+#   backend = "s3"
+#   config = {
+#     key    = var.eks_terraform_state_key
+#     bucket = var.terraform_platform_state_store
+#     region = var.aws_region
+#   }
+# }
 
-data "terraform_remote_state" "route53" {
-  backend = "s3"
-  config = {
-    key    = var.route53_terraform_state_key
-    bucket = var.terraform_platform_state_store
-    region = var.aws_region
-  }
-}
+# data "aws_eks_cluster" "cluster" {
+#   name = "example"
+# }
 
-data "terraform_remote_state" "security_groups_k8s" {
-  backend = "s3"
-  config = {
-    key    = var.security_groups_k8s_terraform_state_key
-    bucket = var.terraform_platform_state_store
-    region = var.aws_region
-  }
-}
 
-data "terraform_remote_state" "security_groups" {
-  backend = "s3"
-  config = {
-    key    = var.security_groups_terraform_state_key
-    bucket = var.terraform_platform_state_store
-    region = var.aws_region
-  }
-}
+# Not used ? Replace with daga.aws_route53_zone ?
+# data "terraform_remote_state" "route53" {
+#   backend = "s3"
+#   config = {
+#     key    = var.route53_terraform_state_key
+#     bucket = var.terraform_platform_state_store
+#     region = var.aws_region
+#   }
+# }
 
+# data "aws_route53_zone" "selected" {
+#   name         = "test.com."
+#   private_zone = true
+# }
+
+#  Not used ?
+# data "terraform_remote_state" "security_groups_k8s" {
+#   backend = "s3"
+#   config = {
+#     key    = var.security_groups_k8s_terraform_state_key
+#     bucket = var.terraform_platform_state_store
+#     region = var.aws_region
+#   }
+# }
+
+#  Not used ?
+# data "terraform_remote_state" "security_groups" {
+#   backend = "s3"
+#   config = {
+#     key    = var.security_groups_terraform_state_key
+#     bucket = var.terraform_platform_state_store
+#     region = var.aws_region
+#   }
+# }
+
+# Not Used but replaced with aws_vpc below
 # data "terraform_remote_state" "vpc" {
 #   backend = "s3"
 #   config = {
@@ -46,27 +61,16 @@ data "terraform_remote_state" "security_groups" {
 #   }
 # }
 
-data "aws_vpcs" "vpcs" {
-  tags = {
-    Name = var.vpc_name
-  }
-}
-
-data "aws_vpc" "vpc" {
-  count = length(data.aws_vpcs.vpcs.ids)
-  id    = tolist(data.aws_vpcs.vpcs.ids)[count.index]
-}
-
 #
 # variable "vpc_name" {}
 #
 # ==============================================================================
 # Terraform state keys and store set by the Make DevOps automation scripts
 
-variable "terraform_platform_state_store" {}
+# variable "terraform_platform_state_store" {}
 
-variable "eks_terraform_state_key" {}
-variable "route53_terraform_state_key" {}
-variable "security_groups_k8s_terraform_state_key" {}
-variable "security_groups_terraform_state_key" {}
-variable "vpc_terraform_state_key" {}
+# variable "eks_terraform_state_key" {}
+# variable "route53_terraform_state_key" {}
+# variable "security_groups_k8s_terraform_state_key" {}
+# variable "security_groups_terraform_state_key" {}
+# variable "vpc_terraform_state_key" {}
--- build/automation/lib/terraform/template/stacks/rds/data.tf
+++ build/automation/lib/terraform/template/stacks/rds/data.tf
@@ -1,25 +1,55 @@
 # ==============================================================================
 # Data
+data "aws_vpcs" "vpcs" {
+  tags = {
+    Name = var.vpc_name
+  }
+}
 
-data "terraform_remote_state" "networking" {
-  backend = "s3"
-  config = {
-    key    = "${var.terraform_state_key_shared}/networking/terraform.state"
-    bucket = var.terraform_state_store
-    region = var.aws_region
+data "aws_vpc" "vpc" {
+  count = length(data.aws_vpcs.vpcs.ids)
+  id    = tolist(data.aws_vpcs.vpcs.ids)[count.index]
+}
+
+data "aws_subnet_ids" "texas_private_subnet_ids" {
+  vpc_id = data.aws_vpc.vpc[0].id
+  tags = {
+    Name = "lk8s-${var.aws_account_name}.texasplatform.uk-private-*"
   }
 }
-data "terraform_remote_state" "terraform-state" {
-  backend = "s3"
-  config = {
-    key    = "${var.terraform_state_key_shared}/terraform-state/terraform.state"
-    bucket = var.terraform_state_store
-    region = var.aws_region
-  }
+
+data "aws_subnet" "texas_subnet" {
+  count = length(data.aws_subnet_ids.texas_subnet_ids.ids)
+  id    = tolist(data.aws_subnet_ids.texas_subnet_ids.ids)[count.index]
 }
+
+data "aws_security_group" "default_vpc_sg" {
+  name = "default"
+}
+
+#  Used by build/automation/lib/terraform/template/stacks/rds/main.tf
+#  replaced by the above data sources
+# data "terraform_remote_state" "networking" {
+#   backend = "s3"
+#   config = {
+#     key    = "${var.terraform_state_key_shared}/networking/terraform.state"
+#     bucket = var.terraform_state_store
+#     region = var.aws_region
+#   }
+# }
+
+#  Not used ?
+# data "terraform_remote_state" "terraform-state" {
+#   backend = "s3"
+#   config = {
+#     key    = "${var.terraform_state_key_shared}/terraform-state/terraform.state"
+#     bucket = var.terraform_state_store
+#     region = var.aws_region
+#   }
+# }
 
 # ==============================================================================
 # Terraform state keys and store set by the Make DevOps automation scripts
 
-variable "terraform_state_store" {}
-variable "terraform_state_key_shared" {}
+# variable "terraform_state_store" {}
+# variable "terraform_state_key_shared" {}
