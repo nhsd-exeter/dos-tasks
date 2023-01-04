b87225e3f592a160b787767c7b69c42abb8fd6b9
Tamara Goldschmidt <tamara.goldschmidt@nhs.net>
2023-01-04 18:12:05 +0000

    DPTS-1009 Cron job changes

--- infrastructure/stacks/referralroles/main.tf
+++ infrastructure/stacks/referralroles/main.tf
@@ -5,8 +5,7 @@ module "referralroles_lambda" {
   image_uri             = "${var.aws_ecr}/${var.project_group_short}/${var.project_name_short}/hk-referralroles:${var.image_version}"
   subnet_ids            = local.private_subnets
   security_group_ids    = [data.aws_security_group.lambda_sg.id]
-  # s3_bucket_arn      = data.aws_s3_bucket.housekeeping_bucket.arn
-  timeout = "900"
+  timeout               = "900"
 
 
   splunk_firehose_subscription = var.splunk_firehose_subscription
