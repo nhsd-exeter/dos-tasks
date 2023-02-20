9bf32953ead93d3bd270cbf195839320e49d93d2
Tim Rickwood <tim.rickwood@nhs.net>
2023-02-20 11:49:57 +0000

    DLS-683 Look up datastore sg by name

--- infrastructure/stacks/security-groups/data.tf
+++ infrastructure/stacks/security-groups/data.tf
@@ -1,5 +1,9 @@
 
 # Look up for rds instance
-data "aws_db_instance" "rds_instance" {
-  db_instance_identifier = var.db_identifier
+# data "aws_db_instance" "rds_instance" {
+#   db_instance_identifier = var.db_identifier
+# }
+
+data "aws_security_group" "datastore" {
+  name = var.db_security_group_name
 }
--- infrastructure/stacks/security-groups/main.tf
+++ infrastructure/stacks/security-groups/main.tf
@@ -7,7 +7,7 @@ resource "aws_security_group" "hk_lambda_sg" {
     from_port       = 5432
     to_port         = 5432
     protocol        = "tcp"
-    security_groups = data.aws_db_instance.rds_instance.vpc_security_groups
+    security_groups = [data.aws_security_group.datastore.id]
   }
   egress {
     description = "AWS API Outbound Access"
@@ -24,7 +24,7 @@ resource "aws_security_group_rule" "db_sg_ingress" {
   from_port                = 5432
   to_port                  = 5432
   protocol                 = "tcp"
-  security_group_id        = element(tolist(data.aws_db_instance.rds_instance.vpc_security_groups), 0)
+  security_group_id        = data.aws_security_group.datastore.id
   source_security_group_id = aws_security_group.hk_lambda_sg.id
   description              = "A rule to allow incoming connections from hk lambda to RDS Security Group"
 }
